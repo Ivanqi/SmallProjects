9502e193183630217413a21d959b1d87af4a0c9c
Ivanqi <512350225@qq.com>
2021-07-27 22:01:06 +0800

    【开发】增加新的链接文件

--- Cosmos/script/lmkfbuild.c
+++ Cosmos/script/lmkfbuild.c
@@ -9,10 +9,9 @@ char* retn_ifilenm(int gc, char* argv[])
 	}
 
 	for(int i=0; i < gc; i++) {
-		if(strcmp("-i",argv[i])==0) {
-			if(i<(gc-1))
-			{
-				return argv[i+1];
+		if (strcmp("-i",argv[i]) == 0){
+			if (i<(gc-1)) {
+				return argv[i + 1];
 			}
 		}
 	}
@@ -21,49 +20,50 @@ char* retn_ifilenm(int gc, char* argv[])
 
 char* retn_ofilenm(int gc,char * argv[])
 {
-	if(gc<1||NULL==argv)
-	{
+	if (gc<1||NULL==argv) {
 		return NULL;
 	}
-	for(int i=0;i<gc;i++)
-	{
-		if(strcmp("-o",argv[i])==0)
-		{
-			if(i<(gc-1))
-			{
-				return argv[i+1];
+
+	for (int i = 0; i < gc; i++) {
+		if (strcmp("-o", argv[i]) == 0) {
+			if (i < (gc - 1)) {
+				return argv[i + 1];
 			}
 		}
 	}
+
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	if(argc<4)
-	{
+	if(argc<4) {
 		printf("pram argc:%d\n", argc);
 		exit(1);
 	}
-	char* ifnm=retn_ifilenm(argc,argv);
-	char* ofnm=retn_ofilenm(argc,argv);
-	FILE* ifp=fopen(ifnm,"r");
-    FILE* ofp=fopen(ofnm,"w+");
-    if (NULL==ifp||NULL==ofp)
-    {
-    	printf("open fail:%s:%s\n", ifnm,ofnm);
+
+	char* ifnm = retn_ifilenm(argc, argv);
+	char* ofnm = retn_ofilenm(argc, argv);
+
+	FILE* ifp = fopen(ifnm, "r");
+    FILE* ofp = fopen(ofnm, "w+");
+
+    if (NULL == ifp || NULL == ofp) {
+    	printf("open fail:%s:%s\n", ifnm, ofnm);
     	exit(1);
     }
-	char fch=fgetc(ifp); 
-	while(fch!=EOF)
-    {
-    	if('['==fch)
-    	{
+
+	char fch = fgetc(ifp); 
+
+	while (fch != EOF) {
+    	if('['==fch) {
     		fch='\t';
     	}
-        fputc(fch,ofp);
-        fch=fgetc(ifp);
+
+        fputc(fch, ofp);
+        fch = fgetc(ifp);
     } 
+    
     fclose(ifp); 
     fclose(ofp);    
 	return 0;
