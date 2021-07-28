53bed0522799a8d2b97a553988cf33eda7ee40c6
Ivanqi <512350225@qq.com>
2021-07-28 20:22:23 +0800

    【开发】增加新的C代码

---
+++ Cosmos/initldr/include/cmctl.h
@@ -0,0 +1,6 @@
+#ifndef CMCTL_H
+#define CMCTL_H
+
+#include "type.h"
+#include "ldrtype.h"
+#endif
---
+++ Cosmos/initldr/include/ldrtype.h
@@ -0,0 +1,7 @@
+#ifndef _LDRTYPE_H
+#define _LDRTYPE_H
+
+#define BFH_RW_R 1
+#define BFH_RW_W 2
+
+#endif
