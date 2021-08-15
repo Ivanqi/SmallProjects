163fafdd43b30db8ca5a7dd7028b51b2f4557b24
Ivanqi <512350225@qq.com>
2021-08-15 22:42:11 +0800

    【开发】增加新代码

--- asyn_network/main.c
+++ asyn_network/main.c
@@ -16,6 +16,7 @@
 
 #include "ae.h"
 #include "anet.h"
+#include "bio.h"
 
 #define REDIS_SERVERPORT        6379                                    // TCP port
 #define REDIS_MAX_QUERYBUF_LEN  10240
@@ -110,6 +111,44 @@ void MainAcceptTcpHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
 
 void MainPriorityRun(struct aeEventLoop *eventLoop) {
     printf("I run befor all other epoll event\n");
+}
+
+int task1RunFunc(void *arg1, void *arg2, void *arg3){
+    printf("task1RunFunc run, %s %s %s\r\n", (char*)arg1, (char*)arg2, (char*)arg3);
+    //通过控制这里的时延可以控制线程池消费任务的速度
+    usleep(100000); //这里通过延时来模拟耗时工作, 业务逻辑自己通过这里来替换sleep
+}
+
+int task2RunFunc(void *arg1, void *arg2, void *arg3){
+    int num1 = 
+    printf("task2RunFunc run, %d %d %d\r\n", *(int*)arg1, *(int*)arg2, *(int*)arg3);
+    //通过控制这里的时延可以控制线程池消费任务的速度
+    usleep(100000); //这里通过延时来模拟耗时工作, 业务逻辑自己通过这里来替换sleep
+}
+
+void threadPoll() {
+    int num1 = 1;
+    int num2 = 2;
+    int num3 = 3;
+
+    bioInit();
+    while(1) {
+        //任务一放入task1线程池中执行
+        if(bioCreateBackgroundJob(BIO_TASK1, "arg1", "arg2", "arg3", task1RunFunc) != 0) {
+            printf("task1 is too much\r\n");
+            goto task2;
+        }
+
+    task2:
+        //任务二放入task2线程池中执行
+        if(bioCreateBackgroundJob(BIO_TASK2, &num1, &num2, &num3, task2RunFunc) != 0) {
+            printf("task2 is too much\r\n");
+        }
+        usleep(100000); //通过调整这里的时延，可以控制生成任务的速度
+    }
+
+    bioKillThreads();
+    return 0;
 }
 
 int main() {
