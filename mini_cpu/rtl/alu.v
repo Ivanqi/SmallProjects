12e99b90536433afcdf19cc361dbd350cb239f99
Ivanqi <512350225@qq.com>
2022-11-17 14:21:35 +0800

    【开发】miniCpu 访存模块

--- mini_cpu/rtl/alu.v
+++ mini_cpu/rtl/alu.v
@@ -1,3 +1,6 @@
+// ALU 模块
+//  1. 执行控制模块根据三类指令产生的 ALU 操作信号 aluOp
+//  2. 在 ALU 模块就能以此为依据，执行相应的运算了
 module alu(
     input[31:0] alu_data1_i,
     input[31:0] alu_data2_i,
@@ -8,35 +11,59 @@ module alu(
 
     reg[31:0] result;
 
+    // sum 信号其实就是两个源操作数的和，不过当运算码 aluOp 的第 3 位和第 1 位为“1”时做的是相减运算
+    // 这是为减法指令或者后面的比较大小而准备的运算
     wire[31:0]  sum = alu_data1_i + ((alu_op_i[3] | alu_op_i[1]) ? -alu_data2_i : alu_data2_i);
+
+    // neq 信号表示的是比较两个操作数是否相等，这就是根据前面的两个操作相减的结果判断
+    // 如果它们的差不为“0”，也就是 sum 信号按位与之后不为“0”，则表示两个操作数不相等
     wire        neq = | sum;
+
+    // cmp 信号表示两个操作数的大小比较，如果它们的最高位（也就是符号位）相等
+    // 则根据两个操作数相减的差值的符号位（也是数值的最高位）判断
+    // 如果是正数，表示源操作数 1 大于源操作数 2，否则表示源操作数 1 小于源操作数 2
+
+    // 如果它们的最高位不相等，则根据 ALU 运算控制码 aluOp 的最低位判断
+    // 如果 aluOp 最低位为“1”，表示是无符号数比较，直接取操作数 2 的最高位作为比较结果
+    // 如果 aluOp 最低位为“0”，表示是有符号数比较，直接取操作数 1 的最高位作为比较结果
     wire        cmp = (alu_data1_i[31] == alu_data2_i[31]) ? sum[31] 
                     : alu_op_i[0] ? alu_data2_i[31] : alu_data1_i[31];
     
+    // 移位操作相关的代码，其中的 shamt 信号是取自源操作数 2 的低五位，表示源操作数 1 需要移多少位（25=32）
+    // shin 信号是取出要移位的数值，根据 aluOp 判断是左移还是右移，如果是右移就直接等于源操作数 1
+    // 如果是左移就先对源操作数的各位数做镜像处理
     wire[4:0]   shamt = alu_data2_i[4:0];
     wire[31:0]  shin  = alu_op_i[2] ? alu_data1_i: reverse(alu_data1_i);
+
+    // shift 信号是根据 aluOp 判断是算术右移还是逻辑右移，如果是算术右移，则在最高位补一个符号位
+    // shiftt 信号是右移之后的结果，这里用到了$signed() 函数对移位前的数据 shift 进行了修饰
+    // $signed() 的作用是决定如何对操作数扩位这个问题
     wire[32:0]  shift = {alu_op_i[3] & shin[31], shin}
+
+    // 在右移操作前，$signed() 函数先把操作数的符号位，扩位成跟结果相同的位宽
+    // 然后再进行移位操作，而 shiftr 就是右移后的结果
     wire[32:0]  shiftt = ($signed(shift) >>> shamt);
     wire[31:0]  shiftr  = shiftt[31:0];
+    // 左移的结果 shiftl，是由右移后的结果进行位置取反得到的
     wire[31:0]  shiftl  = reverse(shiftr);
 
     always @(*) begin
         case(alu_op_i)
-            `ALU_OP_ADD:    result <= sum;
-            `ALU_OP_SUB:    result <= sum;
-            `ALU_OP_SLL:    result <= shiftl;
-            `ALU_OP_SLT:    result <= cmp;
-            `ALU_OP_SLTU:   result <= cmp;
-            `ALU_OP_XOR:    result <= (alu_data1_i ^ alu_data2_i);
-            `ALU_OP_SRL:    result <= shiftr;
-            `ALU_OP_SRA:    result <= shiftr;
-            `ALU_OP_OR:     result <= (alu_data1_i | alu_data2_i);
-            `ALU_OP_AND:    result <= (alu_data1_i & alu_data2_i);
+            `ALU_OP_ADD:    result <= sum;  // 加法运算add
+            `ALU_OP_SUB:    result <= sum;  // 减法运算sub
+            `ALU_OP_SLL:    result <= shiftl;   // 左移位shiftl
+            `ALU_OP_SLT:    result <= cmp;      // 小于
+            `ALU_OP_SLTU:   result <= cmp;      // 小于
+            `ALU_OP_XOR:    result <= (alu_data1_i ^ alu_data2_i);  // 异或
+            `ALU_OP_SRL:    result <= shiftr;   // 右移位shiftr
+            `ALU_OP_SRA:    result <= shiftr;   // 右移位shiftr
+            `ALU_OP_OR:     result <= (alu_data1_i | alu_data2_i);  // 逻辑或|
+            `ALU_OP_AND:    result <= (alu_data1_i & alu_data2_i);  // 逻辑与&
 
-            `ALU_OP_EQ:     result <= {31'b0, ~neq};
-            `ALU_OP_NEQ:    result <= {31'b0, neq};
-            `ALU_OP_GE:     result <= {31'b0, ~cmp};
-            `ALU_OP_GEU:    result <= {31'b0, ~cmp};
+            `ALU_OP_EQ:     result <= {31'b0, ~neq};    // 相等
+            `ALU_OP_NEQ:    result <= {31'b0, neq};     // 不相等 
+            `ALU_OP_GE:     result <= {31'b0, ~cmp};    // 大于
+            `ALU_OP_GEU:    result <= {31'b0, ~cmp};    // 大于
 
             default:        begin
                             result <= 32'b0;
---
+++ mini_cpu/rtl/gen_regs.v
@@ -0,0 +1,39 @@
+// 通用寄存器
+module gen_regs (
+    input clk,
+    input reset,
+    input wen,  // 写控制使能信号
+    input[4:0] regRAddr1, regRAddr2, regWAddr,
+    input[31:0] regWData,
+
+    output[31:0] regRData1,
+    output[31:0] regRData2
+);
+    integer ii;
+    // 通用寄存器总共有 32 个
+    reg[31:0] regs[31:0];
+
+    // write registers
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            for(ii=0; ii < 32; ii=ii+1)
+                regs[ii] <= 32'b0;
+        end
+        else if (wen & (|regWAddr))
+            regs[regWAddr] <= regWData
+    end
+
+    // 因为写寄存器是边沿触发的，在一个时钟周期内写入的寄存器数据，需要在下一个时钟周期才能把写入的数据读取出来
+    // 为了提高读写效率，在对同一个寄存器进行读写时，如果写使能 wen 有效，就直接把写入寄存器的数据送给读数据接口
+    // 这样就可以在一个时钟周期内，读出当前要写入的寄存器数据了
+
+    // read registers
+    // 读地址 regRAddr1 = 5’b0 时的寄存器.把第一个寄存器叫做 0 值寄存器
+    // 因为在 RISC-V 指令架构中就规定好了，第一个通用寄存器必须编码为 0
+    // 也就是把写入该寄存器的数据忽略，而在读取时永远输出为 0
+    assign regRData1 = wen & (regWAddr == regRAddr1) ? regWData
+            : ((regRAddr1 != 5'b0) ? regs[regRAddr1] : 32'b0);
+    
+    assign regRData2 = wen & (regWAddr == regRAddr2) ? regWData
+            : ((regRAddr2 != 5'b0) ? regs[regRAddr2] : 32'b0);
+endmodule
